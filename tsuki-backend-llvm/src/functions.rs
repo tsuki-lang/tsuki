79d02bbe8a4c2cdc01ced76d92c34762084fb375
lqdev <liquidekgaming@gmail.com>
2021-08-27 15:22:59 +0200

    backend-llvm: position allocas at the beginning of the function

--- tsuki-backend-llvm/src/functions.rs
+++ tsuki-backend-llvm/src/functions.rs
@@ -7,6 +7,7 @@ use inkwell::module::Module;
 use inkwell::types::FunctionType;
 use inkwell::values::FunctionValue;
 
+/// Data associated with a single function.
 pub struct Function<'c> {
    pub value: FunctionValue<'c>,
    pub entry_block: BasicBlock<'c>,
@@ -26,11 +27,18 @@ impl<'c> Function<'c> {
    }
 
    /// Positions the given builder at the start of the function.
-   pub fn position_at_entry_block(&self, builder: Builder) {
+   pub fn position_at_entry_block(&self, builder: &Builder) {
       if let Some(instruction) = self.entry_block.get_first_instruction() {
          builder.position_before(&instruction);
       } else {
          builder.position_at_end(self.entry_block);
       }
+   }
+
+   /// Creates a builder that adds instructions to the top of the entry block.
+   pub fn create_entry_block_builder(&self, context: &'c Context) -> Builder<'c> {
+      let builder = context.create_builder();
+      self.position_at_entry_block(&builder);
+      builder
    }
 }
--- tsuki-backend-llvm/src/variables.rs
+++ tsuki-backend-llvm/src/variables.rs
@@ -6,17 +6,20 @@ use tsuki_frontend::sem::Ir;
 
 use crate::CodeGen;
 
+/// Storage for variable values. This maps symbol IDs to `Value`s from LLVM.
 pub(crate) struct Variables<'c> {
    variables: Vec<Option<BasicValueEnum<'c>>>,
 }
 
 impl<'c> Variables<'c> {
+   /// Creates and initializes a new variable value storage.
    pub(crate) fn new() -> Self {
       Self {
          variables: Vec::new(),
       }
    }
 
+   /// Inserts a new value under the given symbol ID.
    fn insert(&mut self, symbol: SymbolId, value: BasicValueEnum<'c>) {
       if self.variables.len() <= symbol.id() {
          self.variables.resize(symbol.id() + 1, None);
@@ -24,6 +27,7 @@ impl<'c> Variables<'c> {
       self.variables[symbol.id()] = Some(value);
    }
 
+   /// Retrieves the value under the given symbol ID.
    fn get(&self, symbol: SymbolId) -> Option<BasicValueEnum<'c>> {
       if symbol.id() >= self.variables.len() {
          None
@@ -34,26 +38,37 @@ impl<'c> Variables<'c> {
 }
 
 impl<'c, 'pm> CodeGen<'c, 'pm> {
+   /// Generates code for a variable reference.
    pub(crate) fn generate_variable_reference(
       &self,
       ir: &Ir,
       node: NodeHandle,
    ) -> BasicValueEnum<'c> {
       let variables = self.variables.borrow();
+
       let symbol_node = ir.ast.first_handle(node);
       let symbol = ir.ast.symbol_id(symbol_node);
+
       let alloca = variables.get(symbol).expect("reference to undeclared variable in IR");
       self.builder.build_load(alloca.into_pointer_value(), ir.symbols.name(symbol))
    }
 
+   /// Generates code for variable declarations.
    pub(crate) fn generate_variable_declaration(&self, ir: &Ir, node: NodeHandle) {
       let mut variables = self.variables.borrow_mut();
+
       let symbol_node = ir.ast.first_handle(node);
       let symbol = ir.ast.symbol_id(symbol_node);
+
       let value_node = ir.ast.second_handle(node);
       let value = self.generate_expression(ir, value_node);
-      let alloca = self.builder.build_alloca(value.get_type(), ir.symbols.name(symbol));
+
+      // A variable declaration always performs an alloca for simplicity's sake.
+      // These allocas, loads, and stores, are optimized by mem2reg later.
+      let builder = self.function.create_entry_block_builder(self.context);
+      let alloca = builder.build_alloca(value.get_type(), ir.symbols.name(symbol));
       self.builder.build_store(alloca, value);
+
       variables.insert(symbol, alloca.as_basic_value_enum());
    }
 }
